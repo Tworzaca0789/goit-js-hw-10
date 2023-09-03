67e5225e7ab2d0b4a97f2e5ffafc9837d6b19cb1
Joanna Matwiej <126902190+Tworzaca0789@users.noreply.github.com>
2023-09-03 09:07:30 +0200

    creating elements

---
+++ src/index.js
@@ -0,0 +1,4 @@
+const breedSelect = document.querySelector('.breed-select');
+const loader = document.querySelector('.loader');
+const error = document.querySelector('.error');
+const catInfo = document.querySelector('.cat-info');
