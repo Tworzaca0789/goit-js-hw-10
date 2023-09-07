d734e746dd3b62a6639e5336bdea40e0d6e7a4e2
Joanna Matwiej <126902190+Tworzaca0789@users.noreply.github.com>
2023-09-07 21:24:49 +0200

    create breeds cats

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,29 +1,29 @@
 import axios from 'axios';
 export { fetchBreeds };
 export { fetchCatByBreed };
+const GET_URL = 'https://api.thecatapi.com/v1';
 
 const API_KEY =
   'live_IPLgjpcZhvWzX0sSEOFtV4lKktVJ8JWuUqlvwj6X4xumqdB0T4XxSASDSIwYovB8';
-
 axios.defaults.headers.common['x-api-key'] = API_KEY;
-//url api o kotach
-const getUrl = 'https://api.thecatapi.com/v1';
 //const res = await axios.get(`some-url/todos`);
-function fetchBreeds() {
-  return axios.get(`${getUrl}/breeds`).then(response => {
-    if (!response.status) {
-      throw new Error(response.status);
-    }
-    return response.data;
-  });
-}
-function fetchCatByBreed(breedId) {
-  return axios
-    .get(`${getUrl}/images/search?breed_ids=${breedId}`)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(response.status);
-      }
+const fetchBreeds = async () => {
+  try {
+    return await axios.get(`${GET_URL}/breeds/`).then(response => {
       return response.data;
     });
-}
+  } catch (error) {
+    throw new Error(response.status);
+  }
+};
+const fetchCatByBreed = async breedId => {
+  try {
+    return await axios
+      .get(`${GET_URL}/images/search?breed_ids=${breedId}`)
+      .then(response => {
+        return response.data;
+      });
+  } catch (error) {
+    throw new Error(response.status);
+  }
+};
--- src/js/index.js
+++ src/js/index.js
@@ -1,84 +1,65 @@
 import axios from 'axios';
+import { Loading } from 'notiflix/build/notiflix-loading-aio';
 import { fetchBreeds, fetchCatByBreed } from './cat-api';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import 'notiflix/dist/notiflix-3.2.6.min.css';
 import SlimSelect from 'slim-select';
 import 'slim-select/dist/slimselect.css';
+const selectors = {
+  breedSelect: document.querySelector('.breed-select'),
+  loader: document.querySelector('.loader'),
+  error: document.querySelector('.error'),
+  catInfo: document.querySelector('.cat-info'),
+};
+selectors.breedSelect.classList.add('is-hidden');
+selectors.loader.classList.add('is-hidden');
+selectors.error.classList.add('is-hidden');
+selectors.catInfo.classList.add('is-hidden');
 
-//tworzenie select
-const breedSelect = document.querySelector('.breed-select');
-const loader = document.querySelector('.loader');
-const error = document.querySelector('.error');
-const catInfo = document.querySelector('.cat-info');
-
-//dodanie klas do ukrycia
-breedSelect.classList.add('is-hidden');
-loader.classList.add('is-hidden');
-error.classList.add('is-hidden');
-catInfo.classList.add('is-hidden');
-
-const arrayObjBreeds = [];
-//kolekcja ras
 fetchBreeds()
-  .then(response => {
-    breedSelect.classList.remove('is-hidden');
-    response.forEach(breed => {
+  .then(data => {
+    selectors.breedSelect.classList.remove('is-hidden');
+    data.forEach(el => {
       const option = document.createElement('option');
-      option.value = breed.id;
-      option.textContent = breed.name;
-      arrayObjBreeds.push(option);
+      option.value = el.id;
+      option.textContent = el.name;
+      selectors.breedSelect.appendChild(option);
+      return option;
     });
-    breedSelect.insertAdjacentHTML('beforeend', arrayObjBreeds);
+
     new SlimSelect({
       select: '.breed-select',
     });
   })
-  .catch(fetchBreedError);
+  .catch(onFetchBreedError);
 
-function fetchBreedError(error) {
+function onFetchBreedError(error) {
   Loading.remove();
   Notify.failure('Oops! Something went wrong! Try reloading the page!', {
-    timeout: 3000,
-    width: '600px',
+    position: 'center-center',
+    width: '400px',
   });
 }
-
 const onSelectBreed = e => {
-  Loading.p.loader('Loading data, please wait...');
-  const selected = selectedOptions[0].value;
+  Loading.arrows('Loading data, please wait...');
+  const selected = e.target.selectedOptions[0];
 
-  fetchCatByBreed(selected)
-    .then(response => {
-      catInfo.innerHTML = `
-  <img src = "${response[0].url}" alt = "${response[0].breeds[0].name}" width = "400"/>
-  <h1>${response[0].breeds[0].name}</h1>
-  <p>${response[0].breeds[0].description}</p>
-  <p>${response[0].breeds[0].temperament}</p>`;
-      Loading.remove();
-      catInfo.classList.remove('is-hidden');
-    })
-    .catch(fetchBreedError);
-};
-breedSelect.addEventListener('change', onSelectBreed);
-/*const loadBreeds = e => {
-  e.preventDefault();
-  const breedName = e.target.breedSelect;
-  fetch(getUrl)
-    .then(response => response.json())
+  fetchCatByBreed(selected.value)
     .then(data => {
-      catInfo.innerHtml = '';
-      catInfo.append(...data);
-    });
+      selectors.catInfo.innerHTML = `
+      <div class="list-div">
+      <div>
+        <img src="${data[0].url}" alt="${data[0].breeds[0].name}" width="400"/>
+        </div>
+        <div>
+        <h1>${data[0].breeds[0].name}</h1>
+        <p>${data[0].breeds[0].description}</p>
+        <p>${data[0].breeds[0].temperament}</p>
+        </div>s
+        </div>`;
+      Loading.remove();
+      selectors.catInfo.classList.remove('is-hidden');
+    })
+    .catch(onFetchBreedError);
 };
-
-breedSelect.addEventListener('change', loadBreeds);
-breedSelect.dispatchEvent(new Event('change'));*/
-/*
-Here's your API key:
-
-live_IPLgjpcZhvWzX0sSEOFtV4lKktVJ8JWuUqlvwj6X4xumqdB0T4XxSASDSIwYovB8
-
-Use it as the 'x-api-key' header when making any request to the API, or by adding as a query string parameter e.g. /n
-'api_key=live_IPLgjpcZhvWzX0sSEOFtV4lKktVJ8JWuUqlvwj6X4xumqdB0T4XxSASDSIwYovB8'
-
-*/
+selectors.breedSelect.addEventListener('change', onSelectBreed);
